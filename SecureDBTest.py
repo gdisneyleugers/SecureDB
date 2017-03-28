66630ebe325b389e5343d74bba46395d70e98f16
gdisneyleugers <gdisneyleugers@users.noreply.github.com>
2017-03-27 18:54:56 -0700

    Update SecureDBTest.py

--- SecureDBTest.py
+++ SecureDBTest.py
@@ -5,16 +5,16 @@ print "KEYS TEST: "
 for i in DbKeys('test.sdb'): print(i)
 cc = DbEntryCount('test.sdb')
 # Encrypt Data
-data = encrypt('signing_key','table_{0}'.format(int(cc+1)), insert_test='passed!', random_data="{}".format(SaltGen(256)), secureDB="is Awesome!")
+data = encrypt('signing_key','table_{0}'.format(int(cc)), insert_test='passed!', random_data="{}".format(SaltGen(256)), secureDB="is Awesome!")
 # Write Data
 DbWrite('test.sdb', data)
 print spacer
 #  Insert Test is a test to test writing to DB
 print "INSERT TEST: "
-print "Entry [{1}]: {0}".format(data,int(cc+1))
+print "Entry [{1}]: {0}".format(data,int(cc))
 # Decrypt Data
 raw = str(decrypt('signing_key',data))
-print "Decrypted [{1}] : {0}".format(raw,int(cc+1))
+print "Decrypted [{1}] : {0}".format(raw,int(cc))
 print spacer
 # Get Test is a test to get data  by id"
 print "GET TEST: "
